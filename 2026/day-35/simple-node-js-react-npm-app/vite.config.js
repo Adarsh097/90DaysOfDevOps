8a62a71eeb6939c2de384972346c2b0c86794627
Afroz-J-Shaikh <afsk1997@gmail.com>
2026-03-02 07:22:46 +0530

    35 done

---
+++ 2026/day-35/simple-node-js-react-npm-app/vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: true,
+    port: 3000,
+    strictPort: true,
+  },
+  build: {
+    outDir: 'build',
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.js',
+  },
+});
